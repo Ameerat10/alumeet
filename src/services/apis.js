879c6e55a052779486a9614f9c8c52d0f859a035
Ameerat Ademuyiwa <a.ademuyiwa@alustudent.com>
2023-12-11 17:48:59 +0400

    More changes

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = "https://alumeet.onrender.com/";
+export const API_URL = "https://alumeet.onrender.com";
 
 axios.defaults.withCredentials = true;
 
